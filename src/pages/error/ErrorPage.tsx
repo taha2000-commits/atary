39060d8ca3a49cbe0c61d25d376d799d6c3d2791
TAHA MAHMOUD <tahamahmoudai@gmail.com>
2026-04-05 18:47:31 +0200

    atary-8: add error page

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,7 +1,17 @@
-
+import { useRouteError } from "react-router";
+type Error = {
+  error: {
+    message: string;
+  };
+};
 const ErrorPage = () => {
+  const { error } = useRouteError() as Error;
 
-  return <div>error</div>;
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-red-600 text-white">
+      {error?.message ? error?.message : "Ooooops Error"}
+    </div>
+  );
 };
 
 export default ErrorPage;
